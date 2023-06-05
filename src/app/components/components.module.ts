23264dab36af9bfc0a29abfcad0a888b3a3f6606
Victor Balbuena <balbuenabellovictorantonio@gmail.com>
2023-06-05 11:23:22 -0600

    Improvements in mobile view
    Added default message

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageBubbleComponent } from './message-bubble/message-bubble.component';
-import { DisplayComponent } from './display/display.component';
 import { ChatsBoxComponent } from './chats-box/chats-box.component';
-import { InputComponent } from './input/input.component';
 import { WritingBubbleComponent } from './writing-bubble/writing-bubble.component';
 import {ReactiveFormsModule} from "@angular/forms";
 
@@ -12,16 +10,12 @@ import {ReactiveFormsModule} from "@angular/forms";
 @NgModule({
   declarations: [
     MessageBubbleComponent,
-    DisplayComponent,
     ChatsBoxComponent,
-    InputComponent,
     WritingBubbleComponent
   ],
   exports: [
     MessageBubbleComponent,
-    DisplayComponent,
     ChatsBoxComponent,
-    InputComponent,
     WritingBubbleComponent
   ],
     imports: [
